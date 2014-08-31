eaa0e2ce0ddc37e1928dfca77113c3f198d10d9d
Toshiki Teramura <toshiki.teramura@gmail.com>
2014-08-31 15:44:39 +0900

    Refine codes

--- Main.hs
+++ Main.hs
@@ -1,33 +1,43 @@
 {-# LANGUAGE OverloadedStrings #-}
 
+import qualified Data.Text as T
+import System.Exit
+
 import Graphics.Vty.Widgets.All
 import Graphics.Vty.LLInput
 import Graphics.Vty.Attributes
-import System.Exit
--- import qualified Data.Text as T
+
+newListView :: [T.Text] -> IO (Widget (List T.Text FormattedText))
+newListView sources = case sources of
+  (x:xs) -> do
+    lst <- newListView xs
+    addToList lst x =<< plainText x
+    return lst
+  [] -> newList (fgColor white) 1
 
 main :: IO()
 main = do
-  -- Header
-  title <- (plainText "alsw: anything.el-like selector widget")
-           <++> (hFill ' ' 1)
-  setNormalAttribute title (black `on` white)
+  -- configures
+  let infoAttr = (black `on` white) -- attribute for header and footer
+  let titleString = "alsw: anything.el-like selector widget"
+  let promptString = "> "
 
-  prompt <- plainText "> "
+  -- Header
+  title <- (plainText titleString) <++> (hFill ' ' 1)
+  setNormalAttribute title infoAttr
+  prompt <- plainText promptString
   e <- editWidget
   header <- (return title) <--> (hBox prompt e)
 
-  -- Footer
-  footer <- (plainText "footer") <++> (hFill ' ' 1)
-  setNormalAttribute footer (black `on` white)
-
   -- List
-  lst <- newList (fgColor white) 1
-  let addPlainText = \ l s -> addToList l s =<< plainText s
+  let sources = ["homhom", "madoka", "saki"]
+  -- let sources = take 1000 $ repeat "homhom"
+  lst <- newListView sources
 
-  addPlainText lst "homhom"
-  addPlainText lst "madoka"
-  addPlainText lst "saki"
+  -- Footer
+  fw <- plainText ("source: " `T.append` "sample")
+  footer <- (return fw) <++> (hFill ' ' 1)
+  setNormalAttribute footer infoAttr
 
   ui <- (return header)
         <--> (return lst)
@@ -38,10 +48,10 @@ main = do
   _ <- addToFocusGroup fg e
   _ <- addToFocusGroup fg lst
 
-  fg `onKeyPressed` \ _ key _ ->
-    if key == KASCII 'q'
-      then exitSuccess
-      else return False
+  fg `onKeyPressed` \ _ key mods ->
+    case (key, mods) of
+      (KASCII 'q', [MCtrl]) -> exitSuccess
+      _ -> return False
 
   -- Collection
   c <- newCollection
